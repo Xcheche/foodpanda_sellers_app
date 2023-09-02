2244de4adddba8b7fd45146add5ce63856ddb3c0
Xcheche <chekwubechukwu23@gmail.com>
2023-09-02 04:21:00 +0100

    In progess

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      title: 'Sellers App',
+      home: const Scaffold(),
+    );
+  }
+}
