aa1951314d10df3760930a5ae20c387fb5b9be12
Xcheche <chekwubechukwu23@gmail.com>
2023-09-07 18:30:39 +0100

    in progress

--- lib/authentication/register.dart
+++ lib/authentication/register.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:foodpanda_sellers_app/widgets/custom_Text_Field.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -8,8 +14,158 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController namecontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+  TextEditingController confirmPasswordcontroller = TextEditingController();
+  TextEditingController phonecontroller = TextEditingController();
+  TextEditingController locationcontroller = TextEditingController();
+  TextEditingController emailcontroller = TextEditingController();
+  XFile? imageXFile;
+  final ImagePicker _picker = ImagePicker();
+
+  Position? position;
+  List<Placemark>? placeMarks;
+  Future<void> _getImage() async {
+    imageXFile = await _picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      imageXFile;
+    });
+  }
+
+  getCurrentLocation() async {
+    Position newPosition = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    position = newPosition;
+    placeMarks = await placemarkFromCoordinates(
+      position!.latitude,
+      position!.longitude,
+    );
+
+    Placemark pMarks = placeMarks![0];
+    String completeAddress = ${pMark.subThoroughfare}, ${pMark.thoroughfare}, ${pMark.subLocality}, ${pMark.locality},${pMark.subAdminstrativeArea}, ${pMark.AdminstrativeArea},${pMark.postalCode},${pMark.country};
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            InkWell(
+              onTap: () {
+                _getImage();
+              },
+              child: CircleAvatar(
+                radius: MediaQuery.of(context).size.width * 0.20,
+                backgroundColor: Colors.white,
+                backgroundImage: imageXFile == null
+                    ? null
+                    : FileImage(
+                        File(imageXFile!.path),
+                      ),
+                child: imageXFile == null
+                    ? Icon(
+                        Icons.add_photo_alternate,
+                        size: MediaQuery.of(context).size.width * 0.20,
+                        color: Colors.grey,
+                      )
+                    : null,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  CustomTextField(
+                    data: Icons.person,
+                    controller: namecontroller,
+                    hintText: "Name",
+                    isObsecre: false,
+                  ),
+                  CustomTextField(
+                    data: Icons.email,
+                    controller: emailcontroller,
+                    hintText: "Email",
+                    isObsecre: false,
+                  ),
+                  CustomTextField(
+                    data: Icons.lock,
+                    controller: passwordcontroller,
+                    hintText: "Password",
+                    isObsecre: true,
+                  ),
+                  CustomTextField(
+                    data: Icons.lock,
+                    controller: confirmPasswordcontroller,
+                    hintText: "Confirm Password",
+                    isObsecre: true,
+                  ),
+                  CustomTextField(
+                    data: Icons.phone,
+                    controller: phonecontroller,
+                    hintText: "Phone",
+                    isObsecre: false,
+                  ),
+                  CustomTextField(
+                    data: Icons.my_location,
+                    controller: locationcontroller,
+                    hintText: "Cafe/Restaurant Address",
+                    isObsecre: false,
+                    enabled: false,
+                  ),
+                  Container(
+                    width: 400,
+                    height: 40,
+                    alignment: Alignment.center,
+                    child: ElevatedButton.icon(
+                      label: const Text(
+                        "Get my Current Location",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      icon: const Icon(
+                        Icons.location_on,
+                        color: Colors.white,
+                      ),
+                      onPressed: () => print("clicked"),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.amber,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () => print("clicked"),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.purple,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+              ),
+              child: const Text(
+                "sign Up",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
