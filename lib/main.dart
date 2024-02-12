57586e00a3e8724cdad6f11ba895780667d60c58
xcheche <chekwubechukwu23@gmail.com>
2024-02-12 18:51:50 +0100

    firebase removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:foodpanda_sellers_app/splashscreen/splash_screen.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
+void main() {
   runApp(const MyApp());
 }
 
